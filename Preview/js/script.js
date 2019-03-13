7ab4e90dc7a85a72fbb67e5b386b23574e8e08a2
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-03-13 15:07:54 +0100

    Update index.html

--- Preview/js/script.js
+++ Preview/js/script.js
@@ -2,6 +2,33 @@
 //navigator.serviceWorker.register('/ProyectoFinalDaw/Preview/sw.js');
 
 //SlideOut
+/*var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+
+//SlideOut - Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+  slideout.open();
+}
+);
+function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });*/
 var slideout = new Slideout({
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
