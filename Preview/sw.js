caedbb22ffdcaf90f8e2c3a3c73dac194ebbf951
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-03-17 14:37:59 +0100

    Update sw.js

--- Preview/sw.js
+++ Preview/sw.js
@@ -55,49 +55,34 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
-});
 
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
 
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
 
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-
-
-
-
-/*self.addEventListener('install', function(event){
-  event.waitUntil(
-    caches.open('GestionComida')
-      .then(function(cache){
-        cache.addAll([
-          '/',
-          '/index.html',
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
-          'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
-          '/css/style.css',
-          '/manifest.json',
-          'https://static.chollometro.com/threads/thread_full_screen/default/11732_1.jpg',
-          'https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js',
-          'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
-          'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-          '/js/script.js'
-        ])
+            return response;
+          }
+        );
       })
-  );
-  return self.clients.claim();
+    );
 });
-
-self.addEventListener('fetch', function(event){
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res){
-        return res;
-      })
-  );
-});
-*/
